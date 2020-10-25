cd08859bab5672404fb1bff6bdcff1c956525bd8
Jolcik <piotr@kostrzenski.eu>
2020-10-25 15:38:04 +0100

    init project

---
+++ src/test/java/com/pkostrzenski/takemine/TakemineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkostrzenski.takemine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakemineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
